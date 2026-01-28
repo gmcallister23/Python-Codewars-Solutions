60a4b39d3094cbfac4f0f003eece793455862198
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-01-28 13:39:14 -0500

    Solutions 1 and 2

--- codewars_solutions.py
+++ codewars_solutions.py
@@ -1,8 +1,13 @@
 #Solution to Even or Odd
-
+def even_or_odd(number):
+        if number % 2 == 0:
+                return "Even"
+        else:
+                return "Odd"
 
 #Solution to Convert a Number to a String
-
+def number_to_string(num):
+        return str(num)
 
 #Remove String Spaces
 
